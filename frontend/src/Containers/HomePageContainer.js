4bb63f0abcd89d0a570476f53f0f203af0aa0e66
unknown <savic.in35.2018@uns.ac.rs>
2022-08-15 21:33:15 +0200

    added pages for login and profile page

---
+++ frontend/src/Containers/HomePageContainer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from '../Components/Common/Footer'
+import HomePage from '../Components/Common/HomePage'
+import Navbar from '../Components/Common/Navbar'
+
+export default function HomePageContainer() {
+  return (
+    <div>
+    <Navbar></Navbar>
+    <HomePage
+
+    ></HomePage>
+    <Footer></Footer>
+  </div>
+  )
+}
---
+++ frontend/src/Containers/LoginContainer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from '../Components/Common/Footer'
+import Login from '../Components/Common/Login'
+import Navbar from '../Components/Common/Navbar'
+
+export default function LoginContainer() {
+  return (
+    <div> <div>
+    <Navbar></Navbar>
+    <Login
+
+    ></Login>
+    <Footer></Footer>
+  </div></div>
+  )
+}
