995567d4fe7f5803dc8204cf986ae5fddac1542b
IlijaRa <rakocevicilija7@gmail.com>
2022-08-21 19:44:59 +0200

    added-JobComment-model

---
+++ AgentService/src/main/java/com/example/AgentService/Model/JobComment.java
@@ -0,0 +1,68 @@
+package com.example.AgentService.Model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "job_comments")
+public class JobComment {
+    private String id;
+    private String pros;
+    private String cons;
+    private double rating;
+    private double salary;
+    private String JobOfferId;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPros() {
+        return pros;
+    }
+
+    public void setPros(String pros) {
+        this.pros = pros;
+    }
+
+    public String getCons() {
+        return cons;
+    }
+
+    public void setCons(String cons) {
+        this.cons = cons;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getJobOfferId() {
+        return JobOfferId;
+    }
+
+    public void setJobOfferId(String jobOfferId) {
+        JobOfferId = jobOfferId;
+    }
+}
